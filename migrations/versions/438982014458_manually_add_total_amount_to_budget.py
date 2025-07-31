a3fc3fb7af2804d3bc979c2cf98322cedce69453
JohnGit916 <johngitonga832@gmail.com>
2025-07-31 10:17:13 -0400

    updates

--- migrations/versions/438982014458_manually_add_total_amount_to_budget.py
+++ migrations/versions/438982014458_manually_add_total_amount_to_budget.py
@@ -18,7 +18,10 @@ depends_on = None
 
 def upgrade():
     op.add_column('budgets', sa.Column('total_amount', sa.Float(), nullable=False, server_default="0"))
+    op.add_column('budgets', sa.Column('created_at', sa.DateTime(), nullable=True))
 
 
 def downgrade():
+    op.drop_column('budgets', 'created_at')
     op.drop_column('budgets', 'total_amount')
+
